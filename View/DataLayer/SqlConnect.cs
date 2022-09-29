fd643af9640ae0d9bba85b58dd3770708327b837
Stijn Versluis <stijn.versluis@gmail.com>
2022-09-29 12:51:12 +0200

    Add SQLconnect file

---
+++ View/DataLayer/SqlConnect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataLayer
+{
+    public class SqlConnect
+    {
+        //variabele aanmaken die in de DAL kunnen worden gebuikt.
+        internal SqlCommand DbCom;
+        internal SqlConnection DBConnection;
+
+        //Methode om connection tussen database op te zetten.
+        public void InitializeDB()
+        {
+            string connectionString = @"Data Source=mssqlstud.fhict.local;Database=dbi482774_vecozoprf;User Id=dbi482774_vecozoprf;Password=VecozoPrf;";
+            DBConnection = new SqlConnection(connectionString);
+            DbCom = DBConnection.CreateCommand();
+        }
+
+        //Connectie opzetten met database.
+        public bool OpenCon()
+        {
+            try
+            {
+                DBConnection.Open();
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        //Connectie afsluiten met database.
+        public bool CloseCon()
+        {
+            try
+            {
+                DbCom.Parameters.Clear();
+                DBConnection.Close();
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+    }
+}
