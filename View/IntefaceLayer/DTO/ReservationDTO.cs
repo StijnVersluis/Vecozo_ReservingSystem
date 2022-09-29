b57a55784ec1bf48aac91b632ace99d5a28623f8
cterkesli <485604@student.fontys.nl>
2022-09-29 15:16:31 +0200

    ReservationDTO

---
+++ View/IntefaceLayer/DTO/ReservationDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntefaceLayer.DTO
+{
+    public class ReservationDTO
+    {
+        public int Id;
+        public int User_id;
+        public int Workzone_id;
+        public DateTimeKind DateTime_Arriving;
+        public DateTime DateTime_Leaving;
+
+        public ReservationDTO(int id, int user_id, int workzone_id, DateTimeKind dateTime_Arriving, DateTime dateTime_Leaving)
+        {
+            this.Id = id;
+            this.User_id = user_id;
+            this.Workzone_id = workzone_id;
+            this.DateTime_Arriving = dateTime_Arriving;
+            this.DateTime_Leaving = dateTime_Leaving;
+        }
+    }
+
+}
