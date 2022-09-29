724dfaf8cfb1971e2ef06602cc1bebcf61c1ed0d
Stijn Versluis <stijn.versluis@gmail.com>
2022-09-29 12:50:45 +0200

    Add Classes User, UserContainer and UserDTO

---
+++ View/BusinessLayer/UserContainer.cs
@@ -0,0 +1,21 @@
+﻿using IntefaceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class UserContainer
+    {
+        private IUserContainer uCont;
+
+        public UserContainer(IUserContainer iUCont) { uCont = iUCont; }
+
+        public User AttemptLogIn(string uName, string password)
+        {
+            return new User(uCont.AttemptLogIn(uName, password));
+        }
+    }
+}
