64bb7fe4860684dfdea5e03aee1723d3cfc1211d
Saeryun3 <samed19982008@hotmail.com>
2022-09-30 11:05:39 +0200

    Reservation

---
+++ View/BusinessLayer/ReservationContainer.cs
@@ -0,0 +1,14 @@
+﻿using IntefaceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class ReservationContainer
+    {
+       private IReservationContainer
+    }
+}
