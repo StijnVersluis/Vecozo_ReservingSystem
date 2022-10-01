32e2dc5bd7d2866378ecf700437ab6e5f094b79b
Stijn Versluis <stijn.versluis@gmail.com>
2022-10-01 14:48:07 +0200

    Team  functions

---
+++ View/DataLayer/TeamDAL.cs
@@ -0,0 +1,28 @@
+﻿using IntefaceLayer;
+using IntefaceLayer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class TeamDAL : SqlConnect, ITeam
+    {
+        public bool AddUser(TeamDTO team, UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateTeam(string name, UserDTO aUser, List<UserDTO> users)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TeamDTO> GetTeams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
