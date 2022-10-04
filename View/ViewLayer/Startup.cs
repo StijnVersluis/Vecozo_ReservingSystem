bf79079fd576c373d50f4a96139fa783f4f2430c
Stijn Versluis <stijn.versluis@gmail.com>
2022-10-04 14:31:38 +0200

    Added login page

--- View/ViewLayer/Startup.cs
+++ View/ViewLayer/Startup.cs
@@ -50,7 +50,7 @@ namespace ViewLayer
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
