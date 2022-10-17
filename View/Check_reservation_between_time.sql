d20068f65d5581ff10ba51467ff997d8f2bca8c7
Stijn Versluis <stijn.versluis@gmail.com>
2022-10-17 11:50:08 +0200

    ...

--- View/Check_reservation_between_time.sql
+++
@@ -1,9 +0,0 @@
-﻿SELECT * FROM Reservations 
-WHERE Workzone_Id = 4 and (
-'2022-10-13 13:51' BETWEEN DateTime_Arriving and DateTime_Leaving
-OR
-'2022-10-13 15:15' BETWEEN DateTime_Arriving and DateTime_Leaving
-OR
-DateTime_Arriving BETWEEN '2022-10-13 13:15' and '2022-10-13 15:51'
-OR
-DateTime_Leaving BETWEEN '2022-10-13 13:15' and '2022-10-13 15:51')
