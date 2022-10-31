524873b7c54d6f7597305a8495cb8b6b95d9a2cb
Tijm van Dongen <486333@student.fontys.nl>
2022-10-31 21:15:48 +0100

    -Improved the Filtering Algorithm

--- View/BusinessLayer/WorkZoneFinder.cs
+++ View/BusinessLayer/WorkZoneFinder.cs
@@ -12,31 +12,62 @@ namespace BusinessLayer
         {
 
         }
-        // er gefilterd op datetime op resrvations waar de workzone id word gebruikt om de workzone uit de lijst te halen
-        // dan word er gefilter op available places voor aantal users
-        
-        public List<Workzone> AvailableWorkzones(List<Workzone> allWorkZones, List<Reservation> allReservations, DateTime dateTime_Arriving, DateTime dateTime_Leaving)
+        /// <summary>
+        /// returns a list of Workzones that are availble between set datetimes and haave enough room for the amount of users
+        /// </summary>
+        /// <param name="allWorkZones"></param>
+        /// <param name="allReservations"></param>
+        /// <param name="Users"></param>
+        /// <param name="dateTime_Arriving"></param>
+        /// <param name="dateTime_Leaving"></param>
+        /// <returns>A list of available and free Workzones</returns>
+        public List<Workzone> AvailableWorkzones(List<Workzone> allWorkZones, List<Reservation> allReservations, List<User> Users, DateTime dateTime_Arriving, DateTime dateTime_Leaving)
         {
-            return GetfreeWorkZones(allWorkZones, GetInterferingReservations(allReservations, dateTime_Arriving, dateTime_Leaving));
+            return GetfreeWorkZones(allWorkZones, GetInterferingReservations(allReservations, dateTime_Arriving, dateTime_Leaving), Users);
         }
-
+        /// <summary>
+        /// Filters the reservations by date to see which are interfering with the given datetimes
+        /// </summary>
+        /// <param name="allReservations"></param>
+        /// <param name="dateTime_Arriving"></param>
+        /// <param name="dateTime_Leaving"></param>
+        /// <returns>Returns interfering Reservations</returns>
         public List<Reservation> GetInterferingReservations(List<Reservation> allReservations, DateTime dateTime_Arriving, DateTime dateTime_Leaving)
         {
             List<Reservation> filteredReservation;
-            filteredReservation = allReservations.Where(reser => reser.DateTime_Arriving <= dateTime_Arriving && reser.DateTime_Arriving >= dateTime_Leaving).ToList(); // checks if any reservation is starting a during the users planned reservation
+            filteredReservation = allReservations.Where(reser => dateTime_Arriving <= reser.DateTime_Arriving && reser.DateTime_Arriving <= dateTime_Leaving).ToList(); // checks if any reservation is starting a during the users planned reservation
             
-            filteredReservation = filteredReservation.Where(reser => reser.DateTime_Leaving >= dateTime_Arriving && reser.DateTime_Leaving <= dateTime_Leaving).ToList(); // checks if any reservation is ending a during the users planned reservation
+            filteredReservation.AddRange(allReservations.Where(reser => dateTime_Arriving  <= reser.DateTime_Leaving && reser.DateTime_Leaving <= dateTime_Leaving).ToList()); // checks if any reservation is ending a during the users planned reservation
            
-            filteredReservation = filteredReservation.Where(reser => (reser.DateTime_Arriving <= dateTime_Arriving && reser.DateTime_Arriving<= dateTime_Leaving) 
+            filteredReservation.AddRange(allReservations.Where(reser => (reser.DateTime_Arriving <= dateTime_Arriving &&  dateTime_Leaving <= reser.DateTime_Arriving) 
                                                                         &&
-                                                                     (reser.DateTime_Leaving >= dateTime_Arriving && reser.DateTime_Leaving >= dateTime_Arriving)).ToList(); // checks if planned reservation is happening during a going reservation
+                                                                     (reser.DateTime_Leaving <= dateTime_Arriving && dateTime_Arriving <= reser.DateTime_Leaving)).ToList()); // checks if planned reservation is happening during a going reservation
             return filteredReservation;
         }
-        public List<Workzone> GetfreeWorkZones(List<Workzone> allWorkZones, List<Reservation> filteredReservation)
+        /// <summary>
+        /// Looks for available workzones based on reservations and the amount of users looking for a spot
+        /// </summary>
+        /// <param name="allWorkZones"></param>
+        /// <param name="filteredReservation"></param>
+        /// <param name="Users"></param>
+        /// <returns>Returns available workzones</returns>
+        public List<Workzone> GetfreeWorkZones(List<Workzone> allWorkZones, List<Reservation> filteredReservation, List<User> Users)
         {
             List<Workzone> UnavailableWorkZones = new();
-            filteredReservation.ForEach(reser => UnavailableWorkZones.Add(allWorkZones.First(workZone => workZone.Id == reser.Workzone_id))); // makes a list of workzones that are taken
-            UnavailableWorkZones.ForEach(workZone => allWorkZones.Remove(workZone)); // removes takenWorkzones from list
+            filteredReservation.ForEach(reser => 
+                                                UnavailableWorkZones.Add( 
+                                                            allWorkZones.First(workZone =>
+                                                                workZone.Id == reser.Workzone_id 
+                                                                &&
+                                                                    (
+                                                                        (filteredReservation.Count(reser =>reser.Workzone_id == workZone.Id) >= workZone.Workspaces) //Checks if there is any workspace available
+                                                                        ||
+                                                                        (workZone.Workspaces - filteredReservation.Count(reser => reser.Workzone_id == workZone.Id)) > Users.Count() //Checks if the availabe workspace is enough for the amount of users
+                                                                    )  
+                                                                )
+                                                            )
+                                                ); // makes a list of workzones that are taken or contains to little workspace for the users 
+            UnavailableWorkZones.ForEach(workZone => allWorkZones.Remove(workZone)); // removes Unavailable Workzones from list
             return allWorkZones;
         }
     }
