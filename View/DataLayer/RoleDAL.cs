4b32c041cd057afd261ac2a273af6e7b8aa23405
Gracia Mamgani <graciamamgani@hotmail.com>
2022-11-17 11:13:36 +0100

    role issue fixed

--- View/DataLayer/RoleDAL.cs
+++ View/DataLayer/RoleDAL.cs
@@ -1,6 +1,8 @@
-﻿using InterfaceLayer;
+﻿using IntefaceLayer.DTO;
+using InterfaceLayer;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +11,57 @@ namespace DataLayer
 {
     public class RoleDAL : SqlConnect, IRoleContainer
     {
-        public RoleDAL() { InitializeDB(); }
+        SqlDataReader reader;
+        public RoleDAL()
+        {
+            InitializeDB();
+        }
+
         public RoleDTO GetRole(int roleId)
         {
-            throw new NotImplementedException();
+            RoleDTO role = null;
+
+            try
+            {
+                OpenCon();
+                DbCom.Parameters.Clear();
+                DbCom.CommandText = "SELECT * FROM Roles WHERE Id = @id";
+                DbCom.Parameters.AddWithValue("id", roleId);
+                reader = DbCom.ExecuteReader();
+                while (reader.Read())
+                {
+                    role = new RoleDTO((int)reader["Id"], (string)reader["Name"]);
+                }
+            }
+            finally
+            {
+                CloseCon();
+            }
+
+            return role;
         }
 
         public List<RoleDTO> GetRoles()
         {
-            throw new NotImplementedException();
+            List<RoleDTO> roles = new();
+
+            try
+            {
+                OpenCon();
+                DbCom.Parameters.Clear();
+                DbCom.CommandText = "SELECT * FROM Roles";
+                reader = DbCom.ExecuteReader();
+                while (reader.Read())
+                {
+                    roles.Add(new RoleDTO((int)reader["Id"], (string)reader["Name"]));
+                }
+            }
+            finally
+            {
+                CloseCon();
+            }
+
+            return roles;
         }
     }
 }
