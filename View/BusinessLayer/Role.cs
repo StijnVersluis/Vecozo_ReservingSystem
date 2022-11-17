af88888d9b5255a9e870913bd63a54e974f25751
Stijn Versluis <stijn.versluis@gmail.com>
2022-11-17 11:02:21 +0100

    Removed Warnings

---
+++ View/BusinessLayer/Role.cs
@@ -0,0 +1,27 @@
+﻿using IntefaceLayer.DTO;
+using InterfaceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Role
+    {
+        private int Id;
+        public string Name { get; set; }
+
+        public Role(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public Role(RoleDTO roleDTO)
+        {
+            Id = roleDTO.Id;
+            Name = roleDTO.Name;
+        }
+    }
+}
---
+++ View/DataLayer/RoleDAL.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class RoleDAL : SqlConnect, IRoleContainer
+    {
+        public RoleDAL() { InitializeDB(); }
+        public RoleDTO GetRole(int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RoleDTO> GetRoles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
